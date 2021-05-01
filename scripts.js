877ee2d9a548fed6e6c441f51c06009400695456
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-01 19:47:47 -0400

    Add functions for open/close lightbox

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const butterfliesButton = document.getElementById("butterfly-btn");
 const mothsButton = document.getElementById("moth-btn");
 const beetlesButton = document.getElementById("beetle-btn");
 
+// Functions for switching between photo groups
 butterfliesButton.addEventListener('click', function() {
     butterflies.style.display = 'flex';
     moths.style.display = 'none';
@@ -24,4 +25,21 @@ beetlesButton.addEventListener('click', function() {
     butterflies.style.display = 'none';
     moths.style.display = 'none';
     beetles.style.display = 'flex';
+})
+
+// Clicking an image opens the lightbox
+const images = document.getElementsByClassName("hover-shadow");
+
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', function() {
+        lightbox.style.display = 'flex';
+    })
+}
+
+// Variables and function for closing lightbox
+const closeButton = document.getElementById("close");
+const lightbox = document.getElementById("lightbox");
+
+closeButton.addEventListener('click', function() {
+    lightbox.style.display = 'none';
 })
