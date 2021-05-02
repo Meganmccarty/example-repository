7bfffab298642a3b04d9ef6324b449438c75b17f
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-01 21:28:25 -0400

    Rewrite lightbox function to include info w/ img

--- scripts.js
+++ scripts.js
@@ -30,10 +30,10 @@ beetlesButton.addEventListener('click', function() {
 // Clicking an image opens the lightbox
 const lightbox = document.getElementById("lightbox");
 const lightboxContent = document.getElementById("lightbox-content")
-const images = document.getElementsByClassName("hover-shadow");
+//const images = document.getElementsByClassName("hover-shadow");
 const closeButton = document.getElementById("close");
 
-
+/*
 for (let i = 0; i < images.length; i++) {
     images[i].addEventListener('click', function() {
         lightbox.style.display = 'flex';
@@ -43,6 +43,31 @@ for (let i = 0; i < images.length; i++) {
         closeButton.addEventListener('click', function() {
             lightbox.style.display = 'none';
             lightboxContent.removeChild(imagesCopy);
+        })
+    })
+}
+*/
+const figures = document.getElementsByClassName("figure");
+
+for (let i = 0; i < figures.length; i++) {
+    let figureImage = figures[i].firstElementChild;
+    let figureDescription = figures[i].lastElementChild;
+    let text = figureDescription.textContent;
+    let imageCopy = figureImage.cloneNode();
+    let descriptionCopy = figureDescription.cloneNode();
+    figureImage.addEventListener('click', function() {
+        lightbox.style.display = 'flex';
+        descriptionCopy.style.display = 'flex';
+        descriptionCopy.textContent = text;
+        lightboxContent.appendChild(imageCopy);
+        lightboxContent.appendChild(descriptionCopy);
+        
+        closeButton.addEventListener('click', function() {
+            lightbox.style.display = 'none';
+            lightboxImage = lightboxContent.firstElementChild;
+            lightboxDescription = lightboxContent.lastElementChild;
+            lightboxContent.removeChild(lightboxImage);
+            lightboxContent.removeChild(lightboxDescription);
         })
     })
 }
