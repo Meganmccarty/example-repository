6bdd79c9d571bb7f555e252fbdb0e825aab94a09
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-01 23:08:38 -0400

    Clean code, add comments

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
-// Divs containing the photos for each insect type
+// Divs containing the photos for each insect type stored in variables
 const butterflies = document.getElementById("butterflies");
 const moths = document.getElementById("moths");
 const beetles = document.getElementById("beetles");
 
-// Buttons for each insect type
+// Buttons for each insect type stored in variables
 const butterfliesButton = document.getElementById("butterfly-btn");
 const mothsButton = document.getElementById("moth-btn");
 const beetlesButton = document.getElementById("beetle-btn");
@@ -27,45 +27,50 @@ beetlesButton.addEventListener('click', function() {
     beetles.style.display = 'flex';
 })
 
-// Clicking an image opens the lightbox
+// Variables for lightbox
 const lightbox = document.getElementById("lightbox");
 const lightboxContent = document.getElementById("lightbox-content")
-//const images = document.getElementsByClassName("hover-shadow");
 const closeButton = document.getElementById("close");
 
-/*
-for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener('click', function() {
-        lightbox.style.display = 'flex';
-        let imagesCopy = images[i].cloneNode()
-        lightboxContent.appendChild(imagesCopy);
-
-        closeButton.addEventListener('click', function() {
-            lightbox.style.display = 'none';
-            lightboxContent.removeChild(imagesCopy);
-        })
-    })
-}
-*/
+// Array containing all divs containing each photo and description
 const figures = document.getElementsByClassName("figure");
 
+// Function for lightbox
 for (let i = 0; i < figures.length; i++) {
+
+    // Get the <img> and <p> elements within div with class name "figure"
     let figureImage = figures[i].firstElementChild;
     let figureDescription = figures[i].lastElementChild;
     let text = figureDescription.textContent;
+
+    // Copy the <img> and <p> elements into new variables
+    // Otherwise, appendChild() will just move the elements on the page instead of copying them
     let imageCopy = figureImage.cloneNode();
     let descriptionCopy = figureDescription.cloneNode();
+
+    // Onclick event for each <img>
     figureImage.addEventListener('click', function() {
+        
+        // Lightbox and description now visible
         lightbox.style.display = 'flex';
         descriptionCopy.style.display = 'flex';
         descriptionCopy.textContent = text;
+
+        // Append <img> and <p> to lightbox
         lightboxContent.appendChild(imageCopy);
         lightboxContent.appendChild(descriptionCopy);
         
+        // Onclick event for close button
         closeButton.addEventListener('click', function() {
+
+            // Lightbox disappears
             lightbox.style.display = 'none';
+
+            // Select <img> and <p> in lightbox
             lightboxImage = lightboxContent.firstElementChild;
             lightboxDescription = lightboxContent.lastElementChild;
+
+            // Remove <img and <p> in lightbox
             lightboxContent.removeChild(lightboxImage);
             lightboxContent.removeChild(lightboxDescription);
         })
